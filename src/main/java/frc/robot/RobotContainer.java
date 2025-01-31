2aa0e6ccf12e054fb359c084d9d848dee71fcc15
hsrobotics <hsrobotics@HS-Robotics-03.ad.gccsmi.org>
2025-01-30 19:47:36 -0500

    Basic vision aiming

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,6 +22,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.commands.Aim;
 import frc.robot.commands.MoveMeters;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -54,8 +55,8 @@ public class RobotContainer {
         config.brightnessScalar = 1; // dim the LEDs to half brightness
         candle.configAllSettings(config);
         candle.setLEDs(255, 255, 255);
-        StrobeAnimation rainbowAnim = new StrobeAnimation(255,0,0,0, 0.9, 64);
-        candle.animate(rainbowAnim);
+        // StrobeAnimation rainbowAnim = new StrobeAnimation(255,0,0,0, 0.9, 64);
+        // candle.animate(rainbowAnim);
         
     }
 
@@ -77,6 +78,7 @@ public class RobotContainer {
         ));
 
         joystick.rightBumper().toggleOnTrue(moveMeterCommand);
+        joystick.rightTrigger().whileTrue(new Aim(drivetrain));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
