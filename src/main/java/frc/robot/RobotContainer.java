18870396ac43fad281aaed7302420aa2fe6ddb54
hsrobotics <hsrobotics@HS-Robotics-03.ad.gccsmi.org>
2025-02-03 17:27:28 -0500

    Added limelight stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -78,7 +78,7 @@ public class RobotContainer {
         ));
 
         joystick.rightBumper().toggleOnTrue(moveMeterCommand);
-        joystick.rightTrigger().whileTrue(new Aim(drivetrain));
+        joystick.rightTrigger().toggleOnTrue(new Aim(drivetrain));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
