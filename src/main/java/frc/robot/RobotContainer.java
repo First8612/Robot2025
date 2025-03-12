225da6334a777118d24de90b416ed466add39077
hsrobotics2 <hsrobotics2@student.gccsmi.org>
2025-03-12 19:50:28 -0400

    Untested Limelight Stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -42,7 +42,6 @@ import frc.robot.subsystems.AlgaeGrabber;
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
-
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.05).withRotationalDeadband(MaxAngularRate * 0.05) // Add a 10% deadband
@@ -70,6 +69,7 @@ public class RobotContainer {
     private final IWannaDumpSomeCoral aprilFollowLeft = new IWannaDumpSomeCoral(drivetrain, true);
     private final IWannaDumpSomeCoral aprilFollowRight = new IWannaDumpSomeCoral(drivetrain, false);
 
+    public double speedMulti = 1;
 
     public RobotContainer() {
         //commands for pathplanner
@@ -92,21 +92,25 @@ public class RobotContainer {
         SmartDashboard.putData("Auto Path", autoChooser);
     }
 
+    void changeSpeed(double newSpeed) {
+        speedMulti = newSpeed;
+    }
+
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-Math.pow(joystickDrive.getLeftY(), 3) * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-Math.pow(joystickDrive.getLeftX(), 3) * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-Math.pow(joystickDrive.getRightX(), 3) * MaxAngularRate) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX(-Math.pow(joystickDrive.getLeftY(), 3) * MaxSpeed * speedMulti) // Drive forward with negative Y (forward)
+                    .withVelocityY(-Math.pow(joystickDrive.getLeftX(), 3) * MaxSpeed * speedMulti) // Drive left with negative X (left)
+                    .withRotationalRate(-Math.pow(joystickDrive.getRightX(), 3) * MaxAngularRate * speedMulti) // Drive counterclockwise with negative X (left)
             )
         );
         joystickDrive.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystickDrive.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystickDrive.getLeftY(), -joystickDrive.getLeftX()))
-        ));
+        
+        joystickDrive.rightBumper().onTrue(new InstantCommand(() -> changeSpeed(0.5)));
+        joystickDrive.rightBumper().whileFalse(new InstantCommand(() -> changeSpeed(1)));
 
         //TEST BOT STUFF
         //joystickDrive.rightBumper().whileTrue(jankyDumper.createDumpCommand());
@@ -144,9 +148,9 @@ public class RobotContainer {
         joystickOperator.leftTrigger().onFalse(new InstantCommand(() -> algaeRoll.runAlgaeIn(0)));
         joystickOperator.rightBumper().onTrue(new InstantCommand(() -> algaeRoll.runAlgaeIn(0)));
 
-        joystickOperator.button(7).whileTrue(new RunCommand(() -> stabber.inFork(-0.05, true)));
+        joystickOperator.button(7).whileTrue(new RunCommand(() -> stabber.inFork(-0.1, true)));
         joystickOperator.button(7).whileFalse(new RunCommand(() -> stabber.inFork(0.1, false)));
-        joystickOperator.a().whileTrue(new RunCommand(() -> stabber.inFork(0.5, true)));
+        joystickOperator.a().whileTrue(new RunCommand(() -> stabber.inFork(0.25, true)));
         joystickOperator.a().whileFalse(new RunCommand(() -> stabber.inFork(0.1, stabber.overrider)));
 
 
@@ -154,11 +158,11 @@ public class RobotContainer {
     }
 
     public void autonomousInit() {
-        ascender.goToPosition(0);
+        //ascender.goToPosition(0);
         extender.setAlgae(0);
     }
     public void teleopInit() {
-        ascender.goToPosition(0);
+        //ascender.goToPosition(0);
         extender.setAlgae(0);
         ascender.pivotMotorLeft.setPosition(0);
         ascender.pivotMotorRight.setPosition(0);
