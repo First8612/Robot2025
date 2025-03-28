d7f604b1ea9a61449ce91e6c9d9bf884bd5a44a1
hsrobotics2 <hsrobotics2@student.gccsmi.org>
2025-03-28 10:53:55 -0400

    AlignTag: Smart dashboard

--- src/main/java/frc/robot/commands/AlignToTag.java
+++ src/main/java/frc/robot/commands/AlignToTag.java
@@ -51,6 +51,7 @@ public class AlignToTag extends Command {
 
     @Override
     public void execute() {
+        SmartDashboard.putBoolean("AlignToTag/targetPose/exists", limelight.hasTarget());
         if (limelight.hasTarget())
             return;
 
